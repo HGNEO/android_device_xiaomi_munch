323e652b79092ddf00caaaecdb925b154e59dbd7
HGNEO <130890333+HGNEO@users.noreply.github.com>
2025-06-24 17:42:51 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ TARGET_RECOVERY_DEVICE_MODULES := init_xiaomi_munch
 TARGET_KERNEL_CONFIG += vendor/xiaomi/munch.config
 
 # Leica Camera
-TARGET_INCLUDES_MIUI_CAMERA := true
+TARGET_INCLUDES_MIUI_CAMERA := false
 
 # OTA assert
 TARGET_OTA_ASSERT_DEVICE := munch,munch_global,munch_in
